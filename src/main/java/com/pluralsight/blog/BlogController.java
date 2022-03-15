91d9ebdc441974bdcdf3d6b98d5907c3bc8ddda5
bruno domingues <bruno.domingues@vilt-group.com>
2022-03-15 09:52:18 +0000

    part 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,7 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         modelMap.put("title", "Blog Post 1");
+        modelMap.put("posts", postRepository.getAllPosts());
         return "home";
     }
 
